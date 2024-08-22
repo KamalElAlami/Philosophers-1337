0b841461a6d9ab3a3493fbc70562bdef31d8894a
Kamal El Alami <kael-ala@e1r4p8.1337.ma>
2024-08-22 21:55:10 +0100

    timestamp

--- routines.c
+++ routines.c
@@ -6,7 +6,28 @@
 /*   By: kael-ala <kael-ala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 17:50:43 by kael-ala          #+#    #+#             */
-/*   Updated: 2024/08/20 17:50:44 by kael-ala         ###   ########.fr       */
+/*   Updated: 2024/08/22 21:50:10 by kael-ala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <./includes/philosophers.h> 
+
+
+
+// void routine_labtal(t_philosopher **abtal)
+// {
+//     // thinking
+//     printf("l batal %d is thinkin\n", (*abtal)->index);
+//     //mutex_lock()
+//     // print taken a fork
+//     // mutex_lock sahbo
+//     //print taken a fork
+//     usleep((*abtal)->info.);
+//     // eating
+//     printf("l batal %d is eatin\n", (*abtal)->index);
+//     ft_mutex((*abtal)->forchette, LOCK);
+//     ft_mutex((*abtal)->next->forchette, LOCK);
+//     usleep((*abtal)->info.time_to_eat);
+    
+//     // sleeping
+// }
